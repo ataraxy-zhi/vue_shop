f28ec819a065f8fe72ac9cd59e1019b7a17c0005
ataraxy-zhi <2711059631@qq.com>
2021-03-06 12:16:19 +0800

    git

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -1,7 +1,9 @@
 // 登录组件
 <template>
 <div class="login_container">
-    login
+    <div class=login_box>
+
+    </div>
 </div>
   
 </template>
@@ -15,7 +17,20 @@ export default {
 <style lang="less" scope>
 .login_container{
     background-color: #2b4b6b;
+    height: 100%;
     
+}
+
+.login_box{
+    width: 450px;
+    height: 300px;
+    background-color: #fff;
+    border-radius: 3px;
+
+    position: absolute;
+    left: 50%;
+    top: 50%;
+    transform: translate(-50%,-50%);
 }
 
 </style>
